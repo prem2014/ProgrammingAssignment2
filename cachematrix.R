65a8c5925456317882cfede8fe9a0b280debfcc5
Prem <prem@beehyv.com>
2015-09-27 11:57:32 +0530

    Formatted and commented solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,14 @@
-## This R script  uses <<- operator which can be used to assign a value to an object 
-## which in its' parent environment. Execute <?"<<-"> in R-console to get better idea.
+## This R script  uses <<- operator, which can be used to assign a value to an object 
+## in its' parent environment. Execute <?"<<-"> or <help("<<-")>in R-console to get better idea.
 
 
 ## Return a list which is used to cache inverse of matrix.
 makeCacheMatrix <- function(x = matrix()) {
         inverseMat <- NULL
         
-        # set matrix and reset the it's inverse value accordingly
+        ## set matrix and reset the it's inverse value accordingly
+        ## setting <x <<- mat> does not create a object <x> locally but it is <x> from parent env
+        ## because of <<- operator.
         set <- function(mat) {
           x <<- mat;
           inverseMat <<- NULL;
@@ -23,15 +25,17 @@ makeCacheMatrix <- function(x = matrix()) {
              )
 }
 
-## Takes <makeCacheMatrix> type in named argument
+## Takes <makeCacheMatrix> type in named argument and assumes that matrix is invertible
 ## Return the inverse of matrix from cache(if there) 
-## else it computes the inverse, cahces it and then returns inverse. 
+## else it computes the inverse, caches it and then returns inverse. 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inverseMat <- x$getInverseMat()
         
         if(!is.null(inverseMat)) {
           message("getting inverse from cached")
+          
+	        # inverse matrix returned from cache		
           return(inverseMat)
         }
         
@@ -41,7 +45,7 @@ cacheSolve <- function(x, ...) {
         # cache the computed inverse matrix
         x$setInverseMat(inverseMat)
         
-        # return inverse matrix
+        # inverse matrix returned
         inverseMat
 }
 
